d84d59a3d77470192b0b896708ffd1d98cefa2b9
Aehad <aeheadjillani@gmail.com>
2024-03-23 20:21:19 +0500

    push my number guessing exercise

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+// 1) computer will generate random number
+// 2) User  input for guessing number
+// 3) compare user input with computer generated number & show result
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+
+const answers = await inquirer.prompt([
+  {
+    name: "userGusessedNumber",
+    type: "number",
+    message: "Please guess a number B/W 1-10:",
+  },
+]);
+if (answers.userGusessedNumber === randomNumber) {
+  console.log("congratulation You Win.");
+} else {
+  console.log("You Lose.");
+}
